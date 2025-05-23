aa126485e9077743adb74941cce1a6fb02ef8ed0
Explotador72 <83897947+Explotador72@users.noreply.github.com>
2025-05-23 16:31:05 +0200

    Add files via upload

--- s.py
+++ s.py
@@ -1,6 +1,9 @@
-import os, spotipy, requests, threading, subprocess, asyncio
+import os, time, glob, shutil, spotipy, requests, threading, subprocess, zipfile, signal, asyncio
 from flask import Flask, send_file, make_response, request
 from spotipy.oauth2 import SpotifyOAuth
+from concurrent.futures import ThreadPoolExecutor
+from yt_dlp import YoutubeDL
+from urllib.parse import quote
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -22,9 +25,106 @@ access_token = token_info['access_token']
 sp = spotipy.Spotify(auth=access_token)
 
 
+def normalize_str(string):
+    return string.translate(str.maketrans('\\/:*?"<>|', "__       "))
+
+
+def download_from_youtube(track, playlist_name):
+    track_name = normalize_str(track["name"]).strip()
+    artist_name = normalize_str(", ".join([a["name"] for a in track["artists"]]))
+    download_path = f"{path_downloads}{playlist_name}"
+
+    os.makedirs(download_path, exist_ok=True)
+    if any(glob.glob(os.path.join(download_path, f"*{track_name}*.mp3"))):
+        return
+
+    ydl_opts = {
+        'quiet': True,
+        'noplaylist': True,
+        'no-warnings': True,
+        'extract_flat': True,
+        'force_generic_extractor': True,
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        try:
+            search_results = ydl.extract_info(f"ytsearch3:{track_name} {artist_name}", download=False).get('entries', [])
+            if not search_results:
+                print(f"No se encontró: {track_name} - {artist_name}")
+                return
+            track_duration = track['duration_ms'] // 1000
+            best_match = min(search_results, key=lambda x: abs(x.get('duration', 0) - track_duration))
+            best_url = best_match['url']
+        except Exception as e:
+            print(f"Error en búsqueda: {str(e)}")
+            return
+
+    print(f"Descargando: {track_name} - {artist_name}\nURL: {best_url}")
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": f"{download_path}/{artist_name} - {track_name}.%(ext)s",
+        "ignoreerrors": True,
+        "postprocessors": [{
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "320",
+        }],
+        'quiet': True,
+        'no_warnings': True
+    }
+
+    for attempt in range(2):
+        try:
+            with YoutubeDL(ydl_opts) as ydl:
+                ydl.download([best_url])
+                normalize_audio(f"{download_path}/{artist_name} - {track_name}.mp3")
+                break
+        except Exception as e:
+            print(e)
+            time.sleep(.5)
+
+
+def normalize_audio(input_file):
+    output_file = input_file.replace(".mp3", "_normalized.mp3")
+    ffmpeg_cmd = [
+        "ffmpeg", "-loglevel", "quiet", "-i", input_file,
+        "-af", "loudnorm=I=-16:LRA=11:TP=-1.5", "-ar", "44100", "-b:a", "320k", output_file
+    ]
+    subprocess.run(ffmpeg_cmd, check=True)
+    os.remove(input_file)
+    os.rename(output_file, input_file)
+    print("Audio normalizado correctamente.")
+
+
+def get_playlist(playlist_url):
+    playlist_id = playlist_url.split("/")[-1].split("?")[0]
+    playlist = sp.playlist(playlist_id)
+    playlist_name = normalize_str(playlist['name']).strip()
+
+    results = sp.playlist_items(playlist_id, limit=100, offset=0)
+    all_tracks = results['items']
+    while results['next']:
+        results = sp.next(results)
+        all_tracks.extend(results['items'])
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for item in all_tracks:
+            executor.submit(download_from_youtube, item['track'], playlist_name)
+
+    zip_path = f'{path_downloads}/{playlist_name}.zip'
+    with zipfile.ZipFile(zip_path, 'w') as zipf:
+        for root, dirs, files in os.walk(f'{path_downloads}/{playlist_name}'):
+            for file in files:
+                zipf.write(os.path.join(root, file), file)
+
+    shutil.rmtree(f"{path_downloads}/{playlist_name}")
+    return zip_path
+
+
 async def upload_file(file_path):
     app = Flask(__name__)
-    port = 5000
+    port = 7987
+
     result = {
         'public_url': None, 'download_event': asyncio.Event(), 'shutdown_event': asyncio.Event()
     }
@@ -41,7 +141,6 @@ async def upload_file(file_path):
     async def start_ngrok():
         if IS_RENDER:
             result['public_url'] = os.getenv("RENDER_EXTERNAL_URL", "http://localhost:5000")
-            print(result['public_url'])
         else:
             ngrok_process = subprocess.Popen(["ngrok", "http", str(port)],
                                              stdout=subprocess.PIPE,
@@ -66,8 +165,8 @@ async def upload_file(file_path):
 
 
 async def set_up():
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(BASE_DIR, "s.py")
-    await upload_file(file_path)
+    url = input('Url')
+    zip_file = get_playlist(url)
+    await upload_file(zip_file)
 
 asyncio.run(set_up())
